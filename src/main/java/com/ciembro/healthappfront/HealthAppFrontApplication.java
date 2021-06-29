3cd51ba1cc02c8226232e032f97637a22077a70c
ciembro <w.ciembroniewicz@gmail.com>
2021-06-29 14:03:11 +0200

    first commit

---
+++ src/main/java/com/ciembro/healthappfront/HealthAppFrontApplication.java
@@ -0,0 +1,13 @@
+package com.ciembro.healthappfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAppFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthAppFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ciembro/healthappfront/HealthAppFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciembro.healthappfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAppFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
